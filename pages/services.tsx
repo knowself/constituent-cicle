0bba9bb64f1fb6012cd7526e015ac53e74d3ea72
Joe Terry <Joe@Bitcoin-Doctor.com>
2024-10-17 20:53:53 -0400

    adding more pages to the website

---
+++ pages/services.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const ServicesPage: React.FC = () => {
+  return (
+    <Layout>
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold mb-6">Our Services</h1>
+        {/* Add content based on https://gorgeous-biscuit-afed8c.netlify.app/services */}
+        {/* This is a placeholder. You'll need to implement the actual content. */}
+      </div>
+    </Layout>
+  );
+};
+
+export default ServicesPage;
